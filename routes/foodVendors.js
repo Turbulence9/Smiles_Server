6c846695bab8ffd0eefac6e34901df9417feb77a
Peter Sarkis <peter@Peters-MacBook-Pro.local>
2018-01-12 13:27:35 -0600

    updated food vendors routes

--- routes/foodVendors.js
+++ routes/foodVendors.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex.js');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 router.get('/', (req, res) => {
   knex('foodvendors').then((foodvendors) => {
@@ -12,33 +14,42 @@ router.post('/login', (req, res) => {
   knex('foodvendors')
   .where({
     email: req.body.email,
-    hashedPassword: req.body.hashedPassword,
   }).first()
   .then(user => {
-    res.send(user);
+    bcrypt.compare(req.body.password, user.hashedPassword)
+    .then((valid) => {
+      if (valid) {
+        res.send(user);
+      } else {
+        res.send('invalid login');
+      }
+    });
   });
 });
 
 router.post('/signup', (req, res) => {
   knex('foodvendors')
-  .insert({
-    businessName: req.body.businessName,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    phoneNumber: req.body.phoneNumber,
-    hashedPassword: req.body.hashedPassword,
-  })
-  .then(() => {
-    res.send('user added');
-  });
-});
-
-router.post('/check', (req, res) => {
-  knex('foodvendors')
-  .where({email:req.body.email}).first()
-  .then(foodvendors => {
-    res.send(foodvendors ? true : false);
+  .where({email:req.body.email})
+  .then(email => {
+    if(email.length === 0){
+      bcrypt.hash(req.body.password, saltRounds)
+      .then((hash) => {
+        knex('foodvendors')
+        .insert({
+          businessName: req.body.businessName,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          phoneNumber: req.body.phoneNumber,
+          hashedPassword: hash,
+        })
+        .then(() => {
+          res.send('user added');
+        });
+      });
+    } else {
+      res.send('email already registered');
+    }
   });
 });
 
